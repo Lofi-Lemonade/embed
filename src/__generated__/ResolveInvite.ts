e44d7a04634a78374ca43d757ed7909e6916b771
Mason Rogers <mason@viction.dev>
2024-02-24 03:41:14 +0000

    Resolve invite

---
+++ src/__generated__/ResolveInvite.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: ResolveInvite
+// ====================================================
+
+export interface ResolveInvite {
+  resolveInvite: string | null;
+}
+
+export interface ResolveInviteVariables {
+  guild: string;
+}
